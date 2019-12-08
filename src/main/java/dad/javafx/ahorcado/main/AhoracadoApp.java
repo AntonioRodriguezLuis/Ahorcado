9849da3c2405ba464f7bf13e11fac4b4ef7675cd
AntonioRodriguez <antoniorodriguezluis99@gmail.com>
2019-12-08 13:55:27 +0000

    cambio de nombre app

--- src/main/java/dad/javafx/ahorcado/main/AhoracadoApp.java
+++ src/main/java/dad/javafx/ahorcado/main/AhoracadoApp.java
@@ -9,11 +9,11 @@ public class AhoracadoApp extends Application {
 	RootController rootController;
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		rootController = new RootController();
+	rootController = new RootController();
 		
 		Scene scene = new Scene(rootController.getView(),600,500);
 		
-		primaryStage.setTitle("Acceso a datos");
+		primaryStage.setTitle("Ahorcado");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
